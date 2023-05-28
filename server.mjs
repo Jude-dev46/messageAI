80802325b1b6fdbf83d4fb81317b51419f0b3b56
Jude Oscar <judeoscar2002gmail.com>
2023-05-29 00:36:49 +0100

    updated server

--- server.mjs
+++ server.mjs
@@ -1,8 +1,12 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+// const cors = require("cors");
+// const express = require("express");
+// const fetch = require("node-fetch");
 const PORT = 8000;
-const cors = require("cors");
-const express = require("express");
 const app = express();
-require("dotenv").config();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
