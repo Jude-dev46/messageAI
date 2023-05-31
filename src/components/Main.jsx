0599c23f0ae194cbd51e4aa3731022be26aab938
Jude Oscar <judeoscar2002gmail.com>
2023-05-31 18:30:03 +0100

    fixed css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = ({
   openSidebar,
 }) => {
   return (
-    <div className="relative w-full max-h-screen flex flex-col justify-between items-center">
+    <div className="relative w-full h-screen max-h-screen flex flex-col justify-between items-center">
       <Nav open={open} openSidebar={openSidebar} />
       <ul className="w-full h-full mt-3 overflow-y-scroll hide-scroll">
         {currChat?.map((currMessage, index) => (
