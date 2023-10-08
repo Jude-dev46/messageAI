7a07f1bf9e0e8a10f5b23e1ed3bde8538b344e9f
Jude Oscar <judeoscar2002gmail.com>
2023-10-08 12:14:19 +0100

    adjusting height

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,11 @@ import { AiOutlineMenu } from "react-icons/ai";
 
 const Nav = ({ openSidebar }) => {
   return (
-    <div className="w-1/2 h-8 flex items-center justify-items-start lg:justify-center gap-10 md:gap-40 mb-2">
-      <div className="mt-4 lg:hidden" onClick={() => openSidebar()}>
+    <div className="w-1/2 h-[10vh] flex items-center justify-items-start lg:justify-center gap-10 md:gap-40 mb-2">
+      <div className="mt-1 lg:hidden" onClick={() => openSidebar()}>
         <AiOutlineMenu size={24} color="#020617" />
       </div>
-      <h1 className="font-sans text-xl text-slate-950 mt-3">MessageAI</h1>
+      <h1 className="font-sans text-xl text-slate-950 mt-1">MessageAI</h1>
     </div>
   );
 };
