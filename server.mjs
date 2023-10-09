f958f9dde11a24dc5899277de6edc580486185ca
Jude Oscar <judeoscar2002gmail.com>
2023-10-09 22:19:12 +0100

    updated cors

--- server.mjs
+++ server.mjs
@@ -17,7 +17,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://messageai.onrender.com" }));
 
 InitiateMongoServer();
 
