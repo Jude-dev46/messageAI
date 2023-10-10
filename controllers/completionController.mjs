1b6c8a42cd590c34a852f76a3100d1f9fc51c9f5
Jude Oscar <judeoscar2002gmail.com>
2023-10-10 08:59:57 +0100

    changed file name

--- controllers/completionController.mjs
+++ controllers/completionController.mjs
@@ -1,4 +1,5 @@
-const fetch = require("node-fetch/src/index");
+import fetch from "node-fetch";
+
 const API_KEY = process.env.API_KEY;
 
 const completionsController = async (req, res) => {
--- routes/completions.js
+++ routes/completions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const completionsController = require("../controllers/completionController");
+const completionsController = require("../controllers/completionController.mjs");
 
 router.post("/", completionsController);
 
