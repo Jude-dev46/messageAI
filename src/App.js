63c2cd5f30c0fe98948f0c6e3c0237c49beb2e45
Jude Oscar <judeoscar2002gmail.com>
2023-10-10 18:50:06 +0100

    setting value

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
 
   const getMessages = async () => {
     const accessToken = localStorage.getItem("token");
+    setValue(valueRef.current.value);
     console.log(value);
     valueRef.current.value = "";
 
