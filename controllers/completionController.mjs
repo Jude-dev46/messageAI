84f9daa2ab56f1ee7e4dfb9c4e617ffc15d81928
Jude Oscar <judeoscar2002gmail.com>
2023-10-10 18:14:12 +0100

    checking completions

--- controllers/completionController.mjs
+++ controllers/completionController.mjs
@@ -3,6 +3,7 @@ import fetch from "node-fetch";
 const API_KEY = process.env.API_KEY;
 
 const completionsController = async (req, res) => {
+  console.log(req.body.message);
   try {
     const gpt = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
